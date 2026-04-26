8a4777c4db9d6a3d84c0903b633e1f200e729777
sure0000 <18243083451@163.com>
2026-04-26 13:21:50 +0800

    fix: force dynamic rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+export const dynamic = 'force-dynamic'
+
 import { useState, useEffect } from 'react'
 import { Todo, Category } from '@/lib/types'
 import { TodoItem } from '@/components/todo/TodoItem'
